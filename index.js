af9ac702d474dc0f72b8c38bbb53ccf9425dc2c0
Artem-Stryzhakov <Phernander1@gmail.com>
2022-11-05 09:47:49 +0200

    ....

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 import mongoose from 'mongoose'
 import { validationResult } from 'express-validator';
 
-
 import { registerValidation } from './validations/auth.js';
 
+import UserModel from './models/User.js';
+
 mongoose.connect(
-    'mongodb+srv://admin:wwwwww@cluster0.t9rcjxm.mongodb.net/?retryWrites=true&w=majority'
-).then(() => console.log("DB ok"))
-    .catch((err) => console.log("DB is not ok", err))
+    'mongodb+srv://artem:WebDevelop@fullstackproject.gx3iiu1.mongodb.net/blog?retryWrites=true&w=majority'
+).then(() => {
+    console.log("DB ok");
+    })
+    .catch((err) => console.log("DB is not ok\n", err))
 
 const app = express();
 
@@ -19,7 +23,7 @@ app.get('/', function (req, res) {
     res.send(`
     <table id="mainTable">
         <tr>
-            <th>Name</th>
+            <th>Name</th> 
             <th>Email</th>
             <th>Age</th>
         </tr>
@@ -29,7 +33,11 @@ app.get('/', function (req, res) {
             <td>18</td>
         </tr>
     </table>
-    <style>   
+    <style>
+        body {
+            background-color: cadetblue;
+        }
+         
         #mainTable {
             width: 50%;
             text-align: center;
@@ -51,15 +59,48 @@ app.get('/', function (req, res) {
     `)
 })
 
-app.post('/auth/register', registerValidation, (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()){
-        return res.status(400).json(errors.array());
-    }
+app.post('/auth/register', registerValidation, async (req, res) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()){
+            return res.status(400).json(errors.array());
+        }
 
-    res.json({
-        success: true,
-    })
+        const password = req.body.password;
+        const salt = await bcrypt.genSalt(10)
+        const hash = await bcrypt.hash(password, salt)
+
+        const doc = new UserModel({
+            email: req.body.email,
+            fullName: req.body.fullName,
+            avatarUrl: req.body.avatarUrl,
+            passwordHash: hash,
+        });
+
+        const user = await doc.save();
+
+        const token = await jwt.sign(
+            {
+            _id: user._id,
+            },
+            'secret123',
+            {
+                expiresIn: "30d",
+            }
+        )
+
+        const { passwordHash, ...userData } = user._doc;
+
+        res.json({
+            ...userData._doc,
+            token,
+        })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: "Не удалось зарегистрироваться.",
+        })
+    }
 })
 
 app.listen(4444, (err) => {
