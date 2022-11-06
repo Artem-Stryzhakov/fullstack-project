816a478fce3ac0c6f716c0eb32b38f6b42f61a60
Artem-Stryzhakov <Phernander1@gmail.com>
2022-11-06 23:10:17 +0200

    working on it...

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import mongoose from 'mongoose'
+import multer from 'multer';
 import {registerValidation, loginValidation, postCreateValidation} from './validations.js';
-import checkAuth from './utils/checkAuth.js';
 
-import * as UserController from './controllers/UserController.js';
-import * as PostController from './controllers/PostController.js';
-import Post from "./models/Post.js";
+import {checkAuth, handleValidationErrors} from './utils/index.js';
+
+import {UserController, PostController} from './controllers/index.js'
 
 mongoose.connect('mongodb+srv://artem:WebDevelop@fullstackproject.gx3iiu1.mongodb.net/blog?retryWrites=true&w=majority')
     .then(() => console.log("DB ok"))
@@ -13,26 +13,40 @@ mongoose.connect('mongodb+srv://artem:WebDevelop@fullstackproject.gx3iiu1.mongod
 
 const app = express();
 
+const storage = multer.diskStorage({
+    destination: (_, __, cb) => {
+        cb(null, 'uploads');
+    },
+    filename: (_, file, cb) => {
+        cb(null, file.originalname);
+    }
+});
+
+const upload = multer({ storage });
+
 app.use(express.json())
+app.use('/uploads', express.static('uploads'))
 
 app.get('/', function (req, res) {
     res.send("Hello world!")
 })
 
-// ===== LOGIN =====
-app.post('/auth/login', loginValidation, UserController.login)
 
-// ===== REGISTER =====
-app.post('/auth/register', registerValidation, UserController.register)
-
-// ===== GET ME =====
+app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login)
+app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register)
 app.get('/auth/me', checkAuth, UserController.getMe)
 
-//app.get('/posts', checkAuth, PostController.getAll)
-//app.get('/posts/:id', checkAuth, PostController.getOne)
-app.post('/posts', checkAuth, postCreateValidation, PostController.create)
-//app.delete('/posts', checkAuth, PostController.remove)
-//app.patch('/posts', checkAuth, PostController.update)
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+    res.json({
+        url: `/upload/${req.file.originalname}`,
+    })
+})
+
+app.get('/posts', PostController.getAll)
+app.get('/posts/:id', PostController.getOne)
+app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create)
+app.delete('/posts/:id', checkAuth, PostController.remove)
+app.patch('/posts/:id', checkAuth, handleValidationErrors, PostController.update)
 
 app.listen(4444, (err) => {
     if (err) {
